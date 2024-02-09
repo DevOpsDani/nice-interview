6e6bec6cdd62081053c47ff07a1dc5df663d721b
Daniel Ben Shushan <danielb630@h-MBP-sl-Daniel.lan>
2024-02-10 01:46:32 +0200

    Added s3 create object

--- tf_config/modules/lambda/lambda.tf
+++ tf_config/modules/lambda/lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda" {
   function_name    = "daniel-interview-nice-devops-interview-lambda"
-  handler          = "index.handler"
+  handler          = "index.lambda_handler"
   runtime          = "python3.9"
   filename         = "lambda_function.zip"
   role             = "arn:aws:iam::557414474363:role/daniel-interview-lambda-execution-role"
