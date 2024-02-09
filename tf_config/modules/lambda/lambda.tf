16d46c2679f35feb84864ef7a12e22b2fd9b2ea2
Daniel Ben Shushan <danielb630@h-MBP-sl-Daniel.lan>
2024-02-09 22:45:32 +0200

    Reconfigure lambda

--- tf_config/modules/lambda/lambda.tf
+++ tf_config/modules/lambda/lambda.tf
@@ -14,9 +14,18 @@ resource "aws_lambda_function" "lambda" {
   role             = "arn:aws:iam::557414474363:role/daniel-interview-lambda-execution-role"
 }
 
-resource "aws_lambda_event_source_mapping" "s3_trigger" {
-  event_source_arn = data.terraform_remote_state.preconfig.outputs.s3_tf_backend_bucket_arn
-  function_name    = aws_lambda_function.lambda.arn
-  batch_size        = 1
-  event = "s3:ObjectCreated:*"
+resource "aws_s3_bucket_notification" "bucket_terraform_notification" {
+   bucket = data.terraform_remote_state.preconfig.outputs.s3_bucket_id
+   lambda_function {
+       lambda_function_arn = aws_lambda_function.lambda.arn
+       events = ["s3:ObjectCreated:*"]
+   }
 }
+
+# resource "aws_lambda_event_source_mapping" "s3_trigger" {
+#   event_source_arn = data.terraform_remote_state.preconfig.outputs.s3_tf_backend_bucket_arn
+#   function_name    = aws_lambda_function.lambda.arn
+#   batch_size       = 1
+#   event            = "s3:ObjectCreated:*"
+# }
+
--- tf_preconfig/outputs.tf
+++ tf_preconfig/outputs.tf
@@ -3,7 +3,12 @@ output "s3_tf_backend_bucket" {
   description = "S3 bucket to manage Terraform state file."
 }
 
-output "s3_tf_backend_bucket_arn" {
-  value       = module.s3.s3_tf_backend_bucket_arn
-  description = "S3 bucket arn."
+
+output "s3_bucket_id" {
+  value = module.s3.s3_bucket_id
 }
+
+# output "s3_tf_backend_bucket_arn" {
+#   value       = module.s3.s3_tf_backend_bucket_arn
+#   description = "S3 bucket arn."
+# }
